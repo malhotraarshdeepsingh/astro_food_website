f84fb354f3cd4f609f647f0febb0a97ce9d0e017
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2024-12-06 17:18:04 +0530

    add cart component with styling and functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import react@2.1.1 from "@astrojs/react";
+import solidJs@2.1.0 from "@astrojs/solid-js";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react@2.1.1(), solidJs@2.1.0()]
+});
